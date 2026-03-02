03ab0a22b00c5bd67b1404bf16c842c4e05d7631
narawaa <nashwa.ghania@ui.ac.id>
2026-03-02 22:30:45 +0700

    chore: add src dll

---
+++ src/modules/GlobamatchPageModule/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const GlobalmatchPageModule = () => {
+  return (
+    <div>
+      Halo Globalmatch
+    </div>
+  )
+}
---
+++ src/modules/HomePageModule/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const HomePageModule = () => {
+  return (
+    <div>
+      Halo
+    </div>
+  )
+}
