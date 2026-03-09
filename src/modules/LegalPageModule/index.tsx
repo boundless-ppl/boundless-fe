78b000f608b349071f36417d015d16a145ecbff9
narawaa <nashwa.ghania@ui.ac.id>
2026-03-09 21:25:09 +0700

    feat: add privacy policy page

---
+++ src/modules/LegalPageModule/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { PrivacyPolicy } from './section/PrivacyPolicy'
+
+export const LegalPageModule = () => {
+  return (
+    <div>
+      <PrivacyPolicy />
+    </div>
+  )
+}
